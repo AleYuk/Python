cbabbab004deec136b7a131875ed5851c6e7f664
AleYuk <uhnoaleksander@gmail.com>
2022-01-17 23:53:53 +0300

    HW_2

--- HW_2.py
+++ HW_2.py
@@ -1,13 +1,11 @@
 # 1. Создать переменную item_1 типа integer.
-item_1 = 8
+item_1 = 4
 # 2. Создать переменную item_2 типа integer.
-item_2 = 1
+item_2 = 3
 # 3. Создать переменную result_sum в которой вы суммируете item_1 и item_2.
-item_sum = item_1 + item_2
+result_sum = item_1 + item_2
 # 4. Вывести result_sum в консоль.
-print('item_1 =', item_1)
-print('item_2 =', item_2)
-print('Summary =',item_sum)
+print('Summary =', result_sum)
 # 5. Создать переменную result_subtr в которой вы вычитаете большую по значению переменную из меньшей по значению.
 result_subtr = max(item_1,item_2) - min(item_1,item_2)
 # 6. Вывести result_subtr в консоль.
@@ -17,7 +15,7 @@ result_multi = item_1 * item_2
 # 8. Вывести result_multi в консоль.
 print('Multiplication =', result_multi)
 # 9. Создать переменную result_exp в которой вы item_1 возводите в степень item_2.
-result_exp = pow(item_1, item_2)
+result_exp = item_1 ** item_2
 # 10.Вывести result_exp в консоль.
 print('Exponentiation =', result_exp)
 # 11. Создать переменную result_m_exp в которой вы item_1 возводите в степень item_2 используя библиотеку math.
@@ -26,19 +24,17 @@ result_m_exp = math.pow(item_1, item_2)
 # 12. Вывести result_m_exp в консоль.
 print('Exponentiation_math =', result_m_exp)
 # 13. Создать переменную result_s_root в которой вы найдёте квадратный корень любой из переменной item
-print('\n', 'Insert item value:')
-item = int(input())
-result_s_root = pow(item, (1/2))
+result_s_root = pow(item_1, (1/2))
 # 16. Вывести result_m_s_root в консоль.
 print('Root', result_s_root)
 # 17. Создать переменную result_mp_s_root в которой вы найдёте квадратный корень любой из переменной item используя библиотеку math используя метод pow.
-result_m_s_root = math.pow(item, (1/2))
+result_m_s_root = math.pow(item_1, (1/2))
 # 18. Вывести result_mp_s_root в консоль.
 print('Root_math', result_m_s_root)
 # 19. Присвоить переменной item_1 odd значение
-item_1 = 8
-# 20. Присвоить переменной item_2 odd значение
-item_2 = 3
+item_1 = 3
+# 20. Присвоить переменной item_2 even значение
+item_2 = 8
 # 21. Создать переменную result_division в которой вы разделите item_1 на item_2.
 result_division = item_1 / item_2
 # 22. Вывести result_division в консоль.
@@ -52,7 +48,7 @@ result_m_ceil = math.ceil(result_division)
 # 26. Вывести result_m_ceil в консоль.
 print('Result_m_ceil =', result_m_ceil)
 # 27. Создать переменную result_int и result_division округлить до ближайшего целого через явное приведение.
-result_int = int(result_division)
+result_int = round(result_division)
 # 28. Вывести result_int в консоль.
 print('Result_int =', result_int)
 # 29. Создать переменную result_no_division_loss в которой вы разделите item_1 на item_2 без остатка.
@@ -110,10 +106,7 @@ b_item_result_multi = b_item_t * b_item_f
 # 55. Вывести b_item_relult_multi в консоль.
 print('b_item_result_multi =', b_item_result_multi)
 # 56. Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f
-try:
-    b_item_result_division = b_item_t / b_item_f
-except:
-    b_item_result_division = 'Error'
+b_item_result_division = b_item_t / b_item_f
 # 57. Вывести b_item_relult_division в консоль. (Получить ошибку)
 print('b_item_result_division =', b_item_result_division)
 # 58. Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int
